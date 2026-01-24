ce79953cd127a512fb012c0f83cb31a1a703f822
Aaron Rhim <rhimaaron@gmail.com>
2026-01-24 13:13:23 -0800

    adding supabase integration

---
+++ temp.md
@@ -0,0 +1,3 @@
+# Will migrate to a proper security documentation tab (future)
+
+Expo-secure-store: Make login token secure so that even though user will automatically login to app (if already logged in) when on phone, other apps cannot access this data.
---
+++ utils/supabase.ts
@@ -0,0 +1,31 @@
+
+// import AsyncStorage from '@react-native-async-storage/async-storage'
+import { createClient, processLock } from '@supabase/supabase-js';
+import * as SecureStore from 'expo-secure-store';
+import 'react-native-url-polyfill/auto';
+
+const ExpoSecureStoreAdapter = {
+  getItem: (key: string) => {
+    return SecureStore.getItemAsync(key);
+  },
+  setItem: (key: string, value: string) => {
+    SecureStore.setItemAsync(key, value);
+  },
+  removeItem: (key: string) => {
+    SecureStore.deleteItemAsync(key);
+  },
+};
+
+export const supabase = createClient(
+  process.env.EXPO_PUBLIC_SUPABASE_URL!,
+  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
+  {
+    auth: {
+      storage: ExpoSecureStoreAdapter,
+      autoRefreshToken: true,
+      persistSession: true,
+      detectSessionInUrl: false,
+      lock: processLock,
+    },
+  })
+        
